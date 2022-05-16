e84aebdc53ee4f6b52de8deee57660bcc9fc4f00
Roland Vincent <roland.vincentnet@gmail.com>
2022-05-17 01:18:19 +0700

    Mengubah Navigation Bar

--- app/src/main/java/com/truesightid/MainActivity.kt
+++ app/src/main/java/com/truesightid/MainActivity.kt
@@ -1,35 +1,41 @@
 package com.truesightid
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.truesightid.databinding.ActivityMainBinding
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import com.truesightid.ui.adapter.ViewPagerAdapter
+import com.truesightid.ui.dashboard.DashboardFragment
+import com.truesightid.ui.home.HomeFragment
+import com.truesightid.ui.notifications.NotificationsFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    lateinit var viewPager: ViewPager;
+    lateinit var tabs: TabLayout;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        viewPager = findViewById<ViewPager>(R.id.viewPager)
+        tabs = findViewById<TabLayout>(R.id.tabs)
 
-        val navView: BottomNavigationView = binding.navView
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.explore_news, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        initTabs()
     }
+
+    private fun initTabs(){
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(DashboardFragment(), "Explore News")
+        adapter.addFragment(HomeFragment(), "News Predict")
+        adapter.addFragment(NotificationsFragment(), "Profile")
+
+        viewPager.adapter = adapter
+        tabs.setupWithViewPager(viewPager)
+
+        tabs.getTabAt(0)!!.setIcon(R.drawable.ic_explore_news_unselected)
+        tabs.getTabAt(1)!!.setIcon(R.drawable.ic_news_predict_unselected)
+        tabs.getTabAt(2)!!.setIcon(R.drawable.ic_profile_unselected)
+    }
+
 }
