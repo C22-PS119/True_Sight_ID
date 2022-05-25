87dbec13fad7e9659cc4a14069d1b5199f38fbfa
wahyurhy <wahyurahayu82@gmail.com>
2022-05-25 10:14:43 +0700

    create layout of fragment_prediction.xml, add style AppButton, add viewModel to NewsPredictFragment.kt, make better searchbar layout with paddingEnd and singleLine, and add Floating Action Button layout

--- app/src/main/java/com/truesightid/ui/explore/ExploreNewsFragment.kt
+++ app/src/main/java/com/truesightid/ui/explore/ExploreNewsFragment.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.truesightid.databinding.FragmentExploreBinding
 import com.truesightid.ui.adapter.ExploreAdapter
@@ -19,7 +17,7 @@ class ExploreNewsFragment : Fragment() {
     private var _binding: FragmentExploreBinding? = null
     private val binding get() = _binding!!
 
-    private val mainViewModel by viewModels<ExploreNewsViewModel>()
+    private val viewModel by viewModels<ExploreNewsViewModel>()
     private lateinit var exploreAdapter: ExploreAdapter
 
     override fun onCreateView(
--- app/src/main/java/com/truesightid/ui/prediction/NewsPredictFragment.kt
+++ app/src/main/java/com/truesightid/ui/prediction/NewsPredictFragment.kt
@@ -4,34 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.truesightid.databinding.FragmentPredictionBinding
+import com.truesightid.ui.explore.ExploreNewsViewModel
 
 class NewsPredictFragment : Fragment() {
 
     private var _binding: FragmentPredictionBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val viewModel by viewModels<NewsPredictViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val newsPredictViewModel =
-            ViewModelProvider(this).get(NewsPredictViewModel::class.java)
 
         _binding = FragmentPredictionBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNewsPredict
-        newsPredictViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textNewsPredict
+//        newsPredictViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
