20f1023fa011aeeb7019defeb00b4f3a09e72f71
wahyurhy <wahyurahayu82@gmail.com>
2022-05-26 09:46:39 +0700

    make layout of fragment_profile.xml

--- app/src/main/java/com/truesightid/ui/prediction/NewsPredictFragment.kt
+++ app/src/main/java/com/truesightid/ui/prediction/NewsPredictFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
@@ -31,6 +32,15 @@ class NewsPredictFragment : Fragment() {
 //            textView.text = it
 //        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.btnPredict.setOnClickListener {
+            Toast.makeText(activity, "Prediction on developing", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
