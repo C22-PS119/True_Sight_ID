4dcc04786818579b874103575ccf37ee8520551a
wahyurhy <wahyurahayu82@gmail.com>
2022-05-27 07:30:28 +0700

    make rotatable layout profile, and make layout of dialog in prediction_dialog.xml, and add some logic in NewsPredictFragment.kt to display the dialog when the button Predict Now is clicked

--- app/src/main/java/com/truesightid/ui/prediction/NewsPredictFragment.kt
+++ app/src/main/java/com/truesightid/ui/prediction/NewsPredictFragment.kt
@@ -1,20 +1,23 @@
 package com.truesightid.ui.prediction
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
+import com.truesightid.R
 import com.truesightid.databinding.FragmentPredictionBinding
-import com.truesightid.ui.explore.ExploreNewsViewModel
 
 class NewsPredictFragment : Fragment() {
 
     private var _binding: FragmentPredictionBinding? = null
     private val binding get() = _binding!!
+    private lateinit var dialog : Dialog
 
     private val viewModel by viewModels<NewsPredictViewModel>()
 
@@ -37,14 +40,26 @@ class NewsPredictFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        dialog = Dialog(requireContext())
 
         binding.btnPredict.setOnClickListener {
-            Toast.makeText(activity, "Prediction on developing", Toast.LENGTH_SHORT).show()
+            predictionDialog()
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun predictionDialog() {
+        dialog.setContentView(R.layout.prediction_dialog)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val close = dialog.findViewById<ImageView>(R.id.iv_close)
+
+        close.setOnClickListener { dialog.dismiss() }
+
+        dialog.show()
     }
 }
