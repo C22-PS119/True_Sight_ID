5a3b0ba6c8f1539eb2232476a6b8611a4131a8f2
wahyurhy <wahyurahayu82@gmail.com>
2022-05-27 10:46:11 +0700

    make new activity EditProfileActivity.kt and add it to AndroidManifest.xml

--- app/src/main/java/com/truesightid/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/truesightid/ui/profile/ProfileFragment.kt
@@ -1,15 +1,14 @@
 package com.truesightid.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import com.truesightid.databinding.FragmentProfileBinding
-import com.truesightid.ui.prediction.NewsPredictViewModel
+import com.truesightid.ui.activity.EditProfileActivity
 
 class ProfileFragment : Fragment() {
 
@@ -23,8 +22,6 @@ class ProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val profileViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -34,6 +31,15 @@ class ProfileFragment : Fragment() {
 //            textView.text = it
 //        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnEditProfile.setOnClickListener {
+            val intent = Intent(requireContext(), EditProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onDestroyView() {
