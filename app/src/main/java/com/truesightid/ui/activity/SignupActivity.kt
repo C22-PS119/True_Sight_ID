f3a196d44ee1fc71764df0ab38aba5e93736fad8
wahyurhy <wahyurahayu82@gmail.com>
2022-05-28 17:35:26 +0700

    create layout SignupActivity.kt

---
+++ app/src/main/java/com/truesightid/ui/activity/SignupActivity.kt
@@ -0,0 +1,28 @@
+package com.truesightid.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.truesightid.databinding.ActivitySignupBinding
+
+class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSignup.setOnClickListener {
+            Toast.makeText(this, "on developed", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.login.setOnClickListener {
+            val intent = Intent(this@SignupActivity, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+        }
+    }
+}
