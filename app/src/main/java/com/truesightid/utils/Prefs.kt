2da94e1c441bd128a36346045feac8278a3c3e63
Wahyu Rahayu <wahyurahayu82@gmail.com>
2022-06-11 14:53:09 +0700

    make this app can change to the dark mode in preference settings

--- app/src/main/java/com/truesightid/utils/Prefs.kt
+++ app/src/main/java/com/truesightid/utils/Prefs.kt
@@ -1,5 +1,7 @@
 package com.truesightid.utils
 
+import android.content.Context
+import android.content.SharedPreferences
 import com.chibatching.kotpref.KotprefModel
 import com.truesightid.data.source.local.entity.UserEntity
 import com.truesightid.utils.extension.toJson
@@ -7,6 +9,10 @@ import com.truesightid.utils.extension.toModel
 
 
 object Prefs : KotprefModel() {
+
+    private const val PREF_NAME = "darkmode.pref"
+    private var sharedPref: SharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+    private val editor: SharedPreferences.Editor = sharedPref.edit()
 
     var isLogin by booleanPref(false)
     var user by stringPref()
@@ -18,6 +24,14 @@ object Prefs : KotprefModel() {
     fun getUser(): UserEntity? {
         if (user.isEmpty()) return null
         return user.toModel(UserEntity::class.java)
+    }
+
+    fun isDarkMode(key: String): Boolean {
+        return sharedPref.getBoolean(key, false)
+    }
+
+    fun setDarkMode(key: String, value: Boolean) {
+        editor.putBoolean(key, value).apply()
     }
 
 }
